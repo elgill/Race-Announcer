cd1ceb1581c92ba145e227b3aab83f26fe139d3d
Chris Gillin <cgillin2@gmail.com>
2023-05-26 18:31:30 -0400

    Refactor main.js

--- main.js
+++ main.js
@@ -1,84 +1,86 @@
-const { app, BrowserWindow , ipcMain, dialog} = require('electron')
-const { Menu } = require('electron')
+const { app, BrowserWindow, ipcMain, dialog, Menu } = require('electron');
 const fs = require('fs');
 const chokidar = require("chokidar");
 
-let win
+let win;
+let watcher;
+let lastLineCount = 0;
 
-let watcher; // Keep track of the current file watcher
-let lastLineCount = 0; // Keep track of the number of lines in the file
+const WINDOW_CONFIG = {
+  width: 1200,
+  height: 800,
+  webPreferences: {
+    nodeIntegration: true,
+    contextIsolation: false,
+    worldSafeExecuteJavaScript: true
+  },
+  icon: __dirname + '/dist/race-announcer-angular/favicon.ico'
+};
 
 function createWindow () {
-  win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      worldSafeExecuteJavaScript: true
-    },
-    icon: __dirname + '/dist/race-announcer-angular/favicon.ico'
-  })
-
+  win = new BrowserWindow(WINDOW_CONFIG)
   win.loadFile('dist/race-announcer-angular/index.html')
-
   win.webContents.openDevTools()
 
   win.on('closed', () => {
     win = null
   })
 
-// Handle IPC message for setting the file path
-  ipcMain.on('set-file-path', (event, filePath) => {
-    // Stop watching the previous file
-    if (watcher) {
-      watcher.close();
-    }
+  setupIPCListeners();
 
-    // Start watching the new file
-    watcher = chokidar.watch(filePath);
-
-    watcher.on('change', (path) => {
-      // Read the file, parse new lines, and send them to the renderer process
-      const data = fs.readFileSync(filePath, 'utf-8');
-      const lines = data.split('\n');
-      const newLines = lines.slice(lastLineCount);
-      lastLineCount = lines.length;
-      win.webContents.send('file-updated', newLines);
-    });
-  });
-
-  // Handle IPC message for opening file dialog
-  ipcMain.handle('open-file-dialog', async (event) => {
-    const result = await dialog.showOpenDialog(win, {
-      properties: ['openFile']
-    });
-
-    if (!result.canceled && result.filePaths.length > 0) {
-      return result.filePaths[0];
-    } else {
-      return null;
-    }
-  });
-
-  app.on('before-quit', () => {
-    if (watcher) {
-      watcher.close(); // stop watching the file when the app is closing
-    }
-  });
+  app.on('before-quit', stopFileWatching);
 }
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
-  app.quit()
-})
+app.on('window-all-closed', app.quit)
 
 app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
 })
+
+function setupIPCListeners() {
+  ipcMain.on('set-file-path', handleFilePathSetting);
+  ipcMain.handle('open-file-dialog', handleOpenFileDialog);
+}
+
+function stopFileWatching() {
+  if (watcher) {
+    watcher.close();
+  }
+}
+
+function handleFilePathSetting(event, filePath) {
+  stopFileWatching();
+
+  watcher = chokidar.watch(filePath);
+  watcher.on('change', handleFileChange(filePath));
+}
+
+function handleFileChange(filePath) {
+  return () => {
+    const data = fs.readFileSync(filePath, 'utf-8');
+    const lines = data.split('\n');
+    const newLines = lines.slice(lastLineCount);
+    lastLineCount = lines.length;
+    win.webContents.send('file-updated', newLines);
+  };
+}
+
+async function handleOpenFileDialog(event) {
+  const result = await dialog.showOpenDialog(win, {
+    properties: ['openFile']
+  });
+
+  if (!result.canceled && result.filePaths.length > 0) {
+    return result.filePaths[0];
+  } else {
+    return null;
+  }
+}
+
 
 const template = [
   {
