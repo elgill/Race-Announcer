7c43cb1e99a479bd458619ba070cccb2cb34d6a2
Chris Gillin <cgillin2@gmail.com>
2023-11-05 14:57:12 -0500

    default to false num lock warning

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -29,7 +29,7 @@ export const DEFAULT_SETTINGS: Settings = {
   deleteKeybind: 'Delete',
   announceTemplate: 'freeform',
   raceStartTime: '',
-  numLockWarn: true,
+  numLockWarn: false,
   customFields: [
     { name: "CustomField1", showInAnnounce: false, showInBrowse: false }
   ],
