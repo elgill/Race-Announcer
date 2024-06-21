e36ee664eaab5b189fe62f8491a8d656be4cd5d2
Chris Gillin <chris@gillin.dev>
2024-06-21 11:02:43 -0400

    Duplicate fix (#23)
    * Initial fix work
    
    * fix duplicate
    
    * version 3
    
    * audit fix
    
    * Version bump
    
    * build packages script

---
+++ buildPackages.sh
@@ -0,0 +1,11 @@
+. ./githubToken.sh
+
+# Windows
+#electron-builder -p always -w
+electron-builder -p always -w --x64
+
+# macOS
+#electron-builder -p always -m --universal
+electron-builder -p always -m
+
+# Linux
